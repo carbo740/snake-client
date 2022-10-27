cbd1880c0af1b8634eecfed542cf32822825a98d
Charles Arboleda <charlesarboleda_@hotmail.com>
2022-10-27 01:05:35 +0000

    Added input.js and refactored

---
+++ input.js
@@ -0,0 +1,19 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = () => {
+
+  if (data === ".") {
+    process.exit();
+  }
+}
+
+module.exports = {
+  setupInput,
+}
--- play.js
+++ play.js
@@ -1,24 +1,6 @@
-const net = require("net");
-const { stdin } = require("process");
 const { connect } = require("./client.js");
+const { setupInput } = require("./input.js");
 
 console.log("Connecting ...");
-connect;
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = () => {
-
-  if (data === '\u0003') {
-    process.exit();
-  }
-}
-
+connect();
 setupInput();
